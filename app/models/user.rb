3f84991668a2489f7e08b92bc7f0ad440af66224
ru2rock <magarisenbei25@gmail.com>
2021-08-05 19:42:15 +0900

    コードレビュー後の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,19 @@ class User < ApplicationRecord
 
   with_options presence: true do
   validates :nickname
-  validates :encrypted_password,:password,:password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "First name is invalid. Input full-width characters"}
-  validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name is invalid. Input full-width characters"}
-  validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "First name kana is invalid. Input full-width katakana characters"}
-  validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Last name kana is invalid. Input full-width katakana characters"}
+  validates :password, :password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :first_name
+  validates :last_name
+  validates :first_name_kana
+  validates :last_name_kana
   validates :birthday
+  end
+  with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+  validates :first_name, {message: "First name is invalid. Input full-width characters"}
+  validates :last_name, {message: "Last name is invalid. Input full-width characters"}
+  end
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+  validates :first_name_kana, {message: "First name kana is invalid. Input full-width katakana characters"}
+  validates :last_name_kana, {message: "Last name kana is invalid. Input full-width katakana characters"}
   end
 end
