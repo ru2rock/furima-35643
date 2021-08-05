8f3ebe5207e8880157a45d5d4330aa234f032eb8
ru2rock <magarisenbei25@gmail.com>
2021-08-05 10:24:50 +0900

    新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nickname
+  validates :encrypted_password,:password,:password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "First name is invalid. Input full-width characters"}
+  validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name is invalid. Input full-width characters"}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "First name kana is invalid. Input full-width katakana characters"}
+  validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Last name kana is invalid. Input full-width katakana characters"}
+  validates :birthday
+  end
 end
