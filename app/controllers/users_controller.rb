1648c11d726944fce3f9634e318690f315e4ef09
ru2rock <magarisenbei25@gmail.com>
2021-09-16 13:11:48 +0900

    マイページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+
   def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+
+    redirect_to new_card_path and return unless card.present?
+
+    customer = Payjp::Customer.retrieve(card.customer_token) #先程のカード情報をもとに、顧客情報を取得
+    @card = customer.cards.first #最初（first）のカード情報を取得する
   end
 
   def update
@@ -8,6 +24,10 @@ class UsersController < ApplicationController
     else
       redirect_to action: "show"
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
