d42cfdc00c17f74b629a362eb59326e4dd9c7089
ru2rock <magarisenbei25@gmail.com>
2021-10-11 19:59:26 +0900

    コメント機能のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :purchases, only: [:index, :create]
+    resources :comments, only: [:create]
   end
   
   resources :users, only: [:show, :update, :new, :create, :edit]
